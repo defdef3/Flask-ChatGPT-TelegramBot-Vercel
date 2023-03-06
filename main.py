1db5d9bd497eb50dd10a3c9f1af89e9f27355a68
defdef3 <126761539+defdef3@users.noreply.github.com>
2023-03-06 15:50:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,6 @@ class ChatGPT:
         
         self.messages = conversation
         self.model = os.getenv("OPENAI_MODEL", default = "gpt-3.5-turbo-0301")
-	self.temperature = os.getenv("OPENAI_TEMPERATURE", default = 2)
 
 
 
@@ -34,6 +33,7 @@ class ChatGPT:
 
         response = openai.ChatCompletion.create(
 	            model=self.model,
+		temperature=0,
                 messages = self.messages
 
                 )
