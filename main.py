24cfadd6e7f6fbc1deeece82b7d1181347c0fb28
defdef3 <126761539+defdef3@users.noreply.github.com>
2023-03-06 15:26:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,8 @@ class ChatGPT:
     def __init__(self):
         
         self.messages = conversation
-        self.model = os.getenv("OPENAI_MODEL", default = "gpt-3.5-turbo")
+        self.model = os.getenv("OPENAI_MODEL", default = "gpt-3.5-turbo-0301")
+	self.temperature = float(os.getenv("OPENAI_TEMPERATURE", default = 2))
 
 
 
